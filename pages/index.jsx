158980791312483f13ac7890209af4627b1eab0d
Evgeny <98524775+bravTm@users.noreply.github.com>
2022-05-22 19:43:25 +0300

    .

---
+++ pages/index.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import styles from "../styles/Home.module.scss";
+import style from "../styles/Form.module.scss";
+
+export default function Home() {
+  const [value, setValue] = useState("");
+  const [list, setList] = useState([]);
+
+  const requestData = async () => {
+    const request = await fetch("https://restcountries.com/v3.1/all");
+    const data = await request.json();
+    setList(data);
+  };
+  requestData();
+
+  const searchCountry = list.filter((country) => {
+    return country.name.common.toLowerCase().includes(value.toLowerCase());
+  });
+  return (
+    <>
+      <div className={styles.parent}>
+        <div className={style.form}>
+          <input
+            type="text"
+            onChange={(e) => setValue(e.target.value)}
+            placeholder="Поиск стран"
+          />
+        </div>
+        {list.length >= 1 ? (
+          searchCountry.map((item, idx) => {
+            return (
+              <div className={styles.container} key={idx}>
+                <p align="center">
+                  <img src={item.flags.png}></img>
+                </p>
+                <h3>{item.name.common}</h3>
+                <h4>Continent - {item.continents}</h4>
+              </div>
+            );
+          })
+        ) : (
+          <h5 className={styles.loading}>Loading ...</h5>
+        )}
+      </div>
+    </>
+  );
+}
